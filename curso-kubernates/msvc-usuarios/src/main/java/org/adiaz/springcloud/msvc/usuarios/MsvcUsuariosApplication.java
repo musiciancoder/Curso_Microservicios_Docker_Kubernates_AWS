af3faea3e6624803358f4c0a018566ea2c1cb98f
musiciancoder <andresguitarra1980@gmail.com>
2022-08-18 11:23:06 -0400

    configurando el contecto de persistencia jpa/hibernate

--- curso-kubernates/msvc-usuarios/src/main/java/org/adiaz/springcloud/msvc/usuarios/MsvcUsuariosApplication.java
+++ curso-kubernates/msvc-usuarios/src/main/java/org/adiaz/springcloud/msvc/usuarios/MsvcUsuariosApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MsvcUsuariosApplication {
 
+	public MsvcUsuariosApplication() {
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(MsvcUsuariosApplication.class, args);
 	}
---
+++ curso-kubernates/msvc-usuarios/src/main/java/org/adiaz/springcloud/msvc/usuarios/models/entity/MsvcUsuariosApplication.java
@@ -0,0 +1,50 @@
+package org.adiaz.springcloud.msvc.usuarios.models.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="usuarios") //opcional, si no se pone la tabla toma el nombre de la clase automaticamente
+public class MsvcUsuariosApplication {
+
+    @Id //llave primaria
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //autoincremental
+    private Long id;
+    private String nombre;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
