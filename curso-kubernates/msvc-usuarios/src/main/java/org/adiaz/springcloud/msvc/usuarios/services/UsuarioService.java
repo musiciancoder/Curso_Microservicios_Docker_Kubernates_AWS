ca0bbba10abc07a3871464a03f88e897b85dc4aa
musiciancoder <andresguitarra1980@gmail.com>
2022-08-18 13:16:36 -0400

    implementando el componente service

---
+++ curso-kubernates/msvc-usuarios/src/main/java/org/adiaz/springcloud/msvc/usuarios/services/UsuarioService.java
@@ -0,0 +1,13 @@
+package org.adiaz.springcloud.msvc.usuarios.services;
+
+import org.adiaz.springcloud.msvc.usuarios.models.entity.Usuario;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UsuarioService { //esta es una interfaz DAO
+    List<Usuario>listar();
+    Optional<Usuario> porId(Long id);
+    Usuario guardar (Usuario usuario);
+    void eliminar (Long id);
+}
---
+++ curso-kubernates/msvc-usuarios/src/main/java/org/adiaz/springcloud/msvc/usuarios/services/UsuarioServiceImpl.java
@@ -0,0 +1,42 @@
+package org.adiaz.springcloud.msvc.usuarios.services;
+
+import org.adiaz.springcloud.msvc.usuarios.models.entity.Usuario;
+import org.adiaz.springcloud.msvc.usuarios.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+//TODO Comparar patron de diseño con el de otros proyectos
+@Service
+public class UsuarioServiceImpl implements UsuarioService { //Clase DAO que implementa interfaz anterior
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Override
+    @Transactional(readOnly = true) //por si hay algun error en proceso de lectura
+    public List<Usuario> listar() {
+        return (List<Usuario>) repository.findAll();
+    }
+
+    @Transactional(readOnly = true) //TODO Dedicarle tiempo a transactional
+    @Override
+    public Optional<Usuario> porId(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    @Transactional
+    public Usuario guardar(Usuario usuario) {
+        return repository.save(usuario);
+    }
+
+    @Override
+    @Transactional
+    public void eliminar(Long id) {
+        repository.deleteById(id);
+    }
+}
