3c5c8d7efb05b8b755adfed8c9b4a86b44cd155f
musiciancoder <andresguitarra1980@gmail.com>
2022-08-19 15:43:19 -0400

    Añadiendo la clase Entity y el CrudRepository

---
+++ curso-kubernates/msvc-cursos/src/main/java/org/adiaz/springcloud/msvc/cursos/entity/Curso.java
@@ -0,0 +1,31 @@
+package org.adiaz.springcloud.msvc.cursos.entity;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name= "cursos")
+public class Curso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
---
+++ curso-kubernates/msvc-cursos/src/main/java/org/adiaz/springcloud/msvc/cursos/services/CursoService.java
@@ -0,0 +1,13 @@
+package org.adiaz.springcloud.msvc.cursos.services;
+
+import org.adiaz.springcloud.msvc.cursos.entity.Curso;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CursoService {
+    List<Curso> listar();
+    Optional<Curso> porId(Long id);
+    Curso guardar(Curso usuario);
+    void eliminar(Long id);
+}
