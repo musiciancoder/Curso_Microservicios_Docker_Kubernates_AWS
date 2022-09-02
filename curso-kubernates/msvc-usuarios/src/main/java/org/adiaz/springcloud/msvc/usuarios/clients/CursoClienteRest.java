d3361d20974994a03d6704aa66c9b7afa064c86b
musiciancoder <andresguitarra1980@gmail.com>
2022-09-02 11:40:10 -0400

    crear docker compose

--- curso-kubernates/msvc-usuarios/src/main/java/org/adiaz/springcloud/msvc/usuarios/clients/CursoClienteRest.java
+++ curso-kubernates/msvc-usuarios/src/main/java/org/adiaz/springcloud/msvc/usuarios/clients/CursoClienteRest.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name="msvc-cursos", url="localhost:8002") //cuando el otro servicio msvc-curso corra en local
-@FeignClient(name="msvc-cursos", url="msvc-cursos:8002") //cuando el otro servicio msvc-curso corra como containeren docker y se puedan comunicar. Notar que msvc-cursos es el --name en los comandos docker
+//@FeignClient(name="msvc-cursos", url="msvc-cursos:8002") //cuando el otro servicio msvc-curso corra como containeren docker y se puedan comunicar. Notar que msvc-cursos es el --name en los comandos docker
+@FeignClient(name="msvc-cursos", url="${msvc.cursos.url}") //msvc.cursos.url desde application.properties
 public interface CursoClienteRest {
 
     @DeleteMapping("/eliminar-curso-usuario/{id}")
